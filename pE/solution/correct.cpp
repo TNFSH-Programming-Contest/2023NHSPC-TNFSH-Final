abe665049fd2a30d6495b9902be8e378427c2ed5
littlecube <froakie20161113@gmail.com>
2023-09-25 01:45:04 +0800

    draft of pACDE

--- pE/solution/correct.cpp
+++ pE/solution/correct.cpp
@@ -1,10 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+string reflect(string s)
+{
+	for (auto &c : s)
+		if (c == 'b' || c == 'p')
+			c ^= ('b' ^ 'p');
+		else
+			c ^= ('d' ^ 'q');
+	return s;
+}
+
+string rotate(string s)
+{
+	for (auto &c : s)
+		if (c == 'b' || c == 'q')
+			c ^= ('b' ^ 'q');
+		else
+			c ^= ('d' ^ 'p');
+	reverse(s.begin(), s.end());
+	return s;
+}
+
+int main()
+{
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		string s, t;
+		cin >> s >> t;
+		if (s == t)
+			cout << "Yes\n";
+		else if (reflect(s) == t)
+			cout << "Yes\n";
+		else if (rotate(s) == t)
+			cout << "Yes\n";
+		else if (reflect(rotate(s)) == t)
+			cout << "Yes\n";
+		else
+			cout << "No\n";
+	}
 }
--- pE/validator/validator.cpp
+++ pE/validator/validator.cpp
@@ -1,16 +1,24 @@
 #include "testlib.h"
 using namespace std;
 
-const int MAXN = 1000;
+const int maxT = 50000;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	registerValidation(argc, argv);
-	// about testlib, see https://codeforces.com/blog/entry/18426
-
-	// TODO
-	inf.readInt(1, MAXN, "n");
+	int T = inf.readInt(1, maxT, "T");
 	inf.readEoln();
+	while (T--)
+	{
+		string s = inf.readString();
+		string t = inf.readString();
+		ensuref(s.size() <= 10, "len s");
+		ensuref(t.size() <= 10, "len t");
+		for (auto c : s)
+			ensuref(c == 'b' || c == 'p' || c == 'd' || c == 'q', "char s");
+		for (auto c : t)
+			ensuref(c == 'b' || c == 'p' || c == 'd' || c == 'q', "char t");
+	}
 	inf.readEof();
-
 	return 0;
 }
